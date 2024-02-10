6cb4253c5fe910ec48f1ffd6adaa8e14bdb0b3bf
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2024-02-10 16:44:23 -0500

    index.html: Se actualizó la estructura básica y se agregó una nueva sección de botones
    style.css: Se aplicaron estilos para ordenar los elementos y distribuir el espacio
    
    script.js: Se actualizó la función setSketch para redimensionar dinámicamente el sketch y se agregó resizeGrid para modificar la cantidad de filas y columnas

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-const container = document.getElementById('container');
-const rows = document.getElementsByClassName('row');
+const sketch = document.getElementById('sketch');
+const resize = document.getElementById('resize');
 
-for (let i = 0;i < 16; i++) {
-    var row = document.createElement('div');
-    row.classList.add("row");
-    container.appendChild(row);
+var rows_grid = 16;
+var cols_grid = 16;
+
+resize.addEventListener('click', resizeGrid);
+
+setSketch();
+
+function setSketch() {
+    sketch.innerHTML = "";
+    var col_size = Math.round(sketch.offsetWidth / cols_grid);
+
+    for (let i = 0;i < rows_grid; i++) {
+        let row = document.createElement('div');
+        row.classList.add("row");
+        sketch.appendChild(row);
+    }
+
+    for(let row = 0; row < rows_grid; row++) {
+        for (let i = 0; i < cols_grid; i++) {
+            let col = document.createElement('div');
+            col.classList.add("col");
+           
+            col.style.width = col_size + "px";
+            col.style.height = col_size + "px";
+            sketch.children[row].appendChild(col);
+        }
+    }
 }
 
-for(let row = 0; row < rows.length; row++) {
-    for (let i = 0; i < 16; i++) {
-        var col = document.createElement('div');
-        col.classList.add("col")
-        rows[i].appendChild(col);
-    }
+function resizeGrid () {
+    let cols = prompt("Enter the number of columns of your sketch");
+    let rows = prompt("Enter the number of rows of your sketch");
+    cols_grid = cols;
+    rows_grid = rows;
+    setSketch();
 }
