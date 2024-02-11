b67969a929370d7b25b9b6310e11bcf280fde7bb
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2024-02-11 13:20:04 -0500

    index.html: Se agregaron clases a los botones y se añadió uno nuevo
    style.css: Se modificó el ancho del sketch para que se adapte mejor y se cambiaron los bordes de los col
    
    script.js: Se adicionaron las funciones getCoordinates y colorSketch, se aplicaron eventos a los botones de aplicación de color

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const sketch = document.getElementById('sketch');
 const resize = document.getElementById('resize');
+const color_buttons = document.querySelectorAll('.color-button');
+const custom_color = document.getElementById('custom');
+const color_picker = document.getElementById('color-picker');
 
+var coordinates;
+var color = "black";
 var rows_grid = 16;
 var cols_grid = 16;
 
 resize.addEventListener('click', resizeGrid);
+custom_color.addEventListener('click', () => {
+    color_picker.click();
+});
+color_picker.addEventListener('input', () => {
+    color = color_picker.value;
+    console.log(color);
+});
+Array.prototype.forEach.call(color_buttons, (color_button) => {
+    color_button.addEventListener('click', () => {
+        color = color_button.id;
+    });
+});
 
 setSketch();
 
@@ -22,12 +39,12 @@ function setSketch() {
         for (let i = 0; i < cols_grid; i++) {
             let col = document.createElement('div');
             col.classList.add("col");
-           
             col.style.width = col_size + "px";
             col.style.height = col_size + "px";
             sketch.children[row].appendChild(col);
         }
     }
+    getCoordinates();
 }
 
 function resizeGrid () {
@@ -36,4 +53,16 @@ function resizeGrid () {
     cols_grid = cols;
     rows_grid = rows;
     setSketch();
+}
+
+function getCoordinates() {
+    coordinates = document.getElementsByClassName('col');
+    Array.from(coordinates).forEach((coordinate) => {
+        coordinate.addEventListener('mouseover', colorSketch);
+    });
+}
+
+function colorSketch (coordinate) {
+    console.log(coordinate.currentTarget.style.backgroundColor, color);
+    coordinate.currentTarget.style.backgroundColor = color;
 }
